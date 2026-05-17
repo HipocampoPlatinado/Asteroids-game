a8b1e5c77ccaf3323f839b777136b88bb6a22e76
Ariel <ivanariel@live.com>
2026-05-17 19:01:07 -0300

    added division asteroids, technically finished

--- Asteroid.py
+++ Asteroid.py
@@ -1,6 +1,9 @@
 from CircleShape import CircleShape
 from constants import LINE_WIDTH
+from constants import ASTEROID_MIN_RADIUS
 import pygame
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -12,3 +15,20 @@ class Asteroid(CircleShape):
     def update(self, dt):
         self.position += self.velocity * dt
 
+    def split(self):
+        self.kill()
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+        asteroids_angle = random.uniform(20,50)
+        first_asteroids_velocity = self.velocity.rotate(asteroids_angle) * 1.2
+        second_asteroids_velocity = self.velocity.rotate(-asteroids_angle)  * 1.2
+
+        child_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        first_child_asteroid = Asteroid(self.position[0], self.position[1], child_radius)
+        second_child_asteroid = Asteroid(self.position[0], self.position[1], child_radius)
+
+        first_child_asteroid.velocity = first_asteroids_velocity
+        second_child_asteroid.velocity = second_asteroids_velocity
